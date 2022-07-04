b6b5cb6bc75063ef94652a17d0db23d863c711ae
Pablo <pablolucio_@hotmail.com>
2022-07-04 07:48:21 -0300

    Added feature to upload files with Multer and read CSV files with node-fs and CsvParser

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,26 +1,26 @@
 import { ISpecificationRepository } from "../../repositories/ISpecificationRepository"
 
-interface IRequest{
+interface IRequest {
     name: string;
     description: string;
 }
 
-class CreateSpecificationUseCase{
+class CreateSpecificationUseCase {
 
-    constructor(private specificationsRepository: ISpecificationRepository){
+    constructor(private specificationsRepository: ISpecificationRepository) {
 
     }
 
-    execute({name, description} : IRequest){
+    execute({ name, description }: IRequest) {
 
         const specificationAlreadyExists = this.specificationsRepository.findByName(name)
 
-        if(specificationAlreadyExists){
+        if (specificationAlreadyExists) {
             throw new Error(`Specification ${name} already exists`)
         }
 
-        this.specificationsRepository.create({ name, description})
+        this.specificationsRepository.create({ name, description })
     }
 }
 
-export {CreateSpecificationUseCase}
+export { CreateSpecificationUseCase }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
-import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoryRepository'
+import multer from 'multer'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+import {importCategoryController} from '../modules/cars/useCases/importCategory'
 
 const categoriesRoutes = Router()
+const upload = multer({
+    dest: './tmp'
+})
 
 categoriesRoutes.post('/', (req, res) => {
     return createCategoryController.handle(req, res)
@@ -11,6 +15,10 @@ categoriesRoutes.post('/', (req, res) => {
 
 categoriesRoutes.get('/', (req, res) => {
     return listCategoriesController.handle(req, res)
+})
+
+categoriesRoutes.post('/imports', upload.single("file"), (req, res) => {
+    return importCategoryController.handle(req, res)
 })
 
 export { categoriesRoutes }
