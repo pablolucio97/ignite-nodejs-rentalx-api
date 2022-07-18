2df2cc3a373f3644f8cbc4da16cc67083e298167
Pablo <pablolucio_@hotmail.com>
2022-07-18 06:49:39 -0300

    chore: turned models on entities

--- src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from "../../models/Specification";
+import { Specification } from "../../entities/Specification";
 import { ISpecificationRepository, ICreateSpecificationDTO } from "../ISpecificationRepository";
 
 class SpecificationRepository implements ISpecificationRepository {
